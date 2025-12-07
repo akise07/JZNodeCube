ec68809c64e91073e84996b0b4d5bb41a5143c06
灬秋落丿 <1492354280@qq.com>
2025-12-07 16:23:43 +0800

    1.0.7 上步补全

--- JZNodeCube/nodes/group.py
+++ JZNodeCube/nodes/group.py
@@ -20,19 +20,65 @@ class GroupNode2(BaseNode2, GroupNode):
     def __init__(self):
         super().__init__()
     
-    def delete_input(self, port):
-        if type(port) in [int, str]:
-            port = self.get_input(port)
-            if port is None:
-                return
+    # def delete_input(self, port):
+    #     if type(port) in [int, str]:
+    #         port = self.get_input(port)
+    #         if port is None:
+    #             return
 
-        if self.is_expanded:
-            sub_graph: SubGraph = self.get_sub_graph()
-            port_node = sub_graph.get_node_by_port(port)
-            if port_node:
-                sub_graph.remove_node(port_node, push_undo=False)
+    #     if self.is_expanded:
+    #         sub_graph: SubGraph = self.get_sub_graph()
+    #         port_node = sub_graph.get_node_by_port(port)
+    #         if port_node:
+    #             sub_graph.remove_node(port_node, push_undo=False)
 
-        super(GroupNode, self).delete_input(port)
+    #     super(GroupNode, self).delete_input(port)
+    
+    # def delete_output(self, port):
+    #     if type(port) in [int, str]:
+    #         port = self.get_output(port)
+    #         if port is None:
+    #             return
+
+    #     if self.is_expanded:
+    #         sub_graph = self.get_sub_graph()
+    #         port_node = sub_graph.get_node_by_port(port)
+    #         if port_node:
+    #             sub_graph.remove_node(port_node, push_undo=False)
+
+    #     super(GroupNode, self).delete_output(port)
+    
+    def set_ports(self, port_data):
+        if not self.port_deletion_allowed():
+            raise PortError(
+                'Ports cannot be set on this node because '
+                '"set_port_deletion_allowed" is not enabled on this node.')
+        
+        for port in self._inputs:
+            self.delete_input(port)
+            # self._view.delete_input(port.view)
+            port.model.node = None
+        for port in self._outputs:
+            self.delete_output(port)
+            # self._view.delete_output(port.view)
+            port.model.node = None
+        self._inputs = []
+        self._outputs = []
+        self._model.outputs = {}
+        self._model.inputs = {}
+
+        [self.add_input(name=port['name'],
+                        multi_input=port['multi_connection'],
+                        display_name=port['display_name'],
+                        locked=port.get('locked') or False)
+        for port in port_data['input_ports']]
+        [self.add_output(name=port['name'],
+                        multi_output=port['multi_connection'],
+                        display_name=port['display_name'],
+                        locked=port.get('locked') or False)
+        for port in port_data['output_ports']]
+        self._view.draw_node()
+    
     
     def __getattr__(self, name):
         try:
